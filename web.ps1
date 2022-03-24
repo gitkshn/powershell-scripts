b2fb0614b37811384ed9fe235a3322e6dbe54ffa
Kasper <kshn2008@yahoo.dk>
2022-03-24 21:42:13 +0100

    added web react hotkey. Remember to add Set-Alias.. command to ps profile

---
+++ web.ps1
@@ -0,0 +1,5 @@
+Write-Output "Opening React Web App, and duplicate tab for Github";
+
+# Use --useApplicationTitle to use the folder name again
+wt --window 0 new-tab -d "C:\Users\Kasper\Documents\GitHub\kshn-web-react" --title Web-Git --suppressApplicationTitle --tabColor "#FFD700";
+wt --window 1 new-tab -d "C:\Users\Kasper\Documents\GitHub\kshn-web-react" --title Web-React --suppressApplicationTitle --tabColor "#1AA7EC";
